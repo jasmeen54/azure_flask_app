7101cbddeac457c6c1dc93070a0b7f35a28ce50b
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-04-25 11:04:32 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask_restful import Api
 
 from .profiles import profiles_blueprint
 from .api.profile import ProfileResource
+from .telemetry import telemetry_bp
 
 app = Flask(__name__)
 api = Api(app, prefix="/api")
 
 app.register_blueprint(profiles_blueprint)
+app.register_blueprint(telemetry_bp)
 
 api.add_resource(ProfileResource, "/profiles")
 
